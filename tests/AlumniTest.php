1226c1a49028140aab5d31307e95016029bb0970
BePsvPT <bepsvpt@protonmail.com>
2019-05-20 19:07:47 +0800

    update tests

--- tests/AlumniTest.php
+++ tests/AlumniTest.php
@@ -2,41 +2,55 @@
 
 namespace CCUPLUS\Authentication\Tests;
 
-use \Mockery;
-use PHPUnit\Framework\TestCase;
 use CCUPLUS\Authentication\EntryPoints\Alumni;
+use GuzzleHttp\ClientInterface;
 use GuzzleHttp\Cookie\CookieJar;
 use GuzzleHttp\Psr7\Response;
-use GuzzleHttp\ClientInterface;
+use Mockery;
+use PHPUnit\Framework\TestCase;
 
 class AlumniTest extends TestCase
 {
-	/**
-	 * @dataProvider provider
-	 */
-	public function test_sign_in($location, $signed)
-	{
-		$header = ['location' => $location];
-		$response = new Response(200, $header, '', '1.1');
+    public function test_sign_in_success()
+    {
+        $response = new Response(200, ['location' => 'https://miswww1.ccu.edu.tw/alumni/alumni/mainmenu.php']);
 
-		$guzzle = Mockery::mock(ClientInterface::class);
-		$guzzle->shouldReceive('request')
-			->once()
-			->andReturn($response);
+        $guzzle = Mockery::mock(ClientInterface::class);
 
-		$alumni = new Alumni($guzzle);
+        $guzzle->shouldReceive('request')
+            ->once()
+            ->andReturn($response);
 
-		$this->assertEquals(
-			$signed,
-			($alumni->signIn('A190135784', '1234') instanceof \GuzzleHttp\Cookie\CookieJar)
-		);
-	}
+        $signedIn = (new Alumni($guzzle))->signIn('A190135784', '1234');
 
-	public function provider()
-	{
-		return [
-			['https://miswww1.ccu.edu.tw/alumni/alumni/mainmenu.php', true],
-			['https://miswww1.ccu.edu.tw/alumni/alumni/login.php', false]
-		];
-	}
+        $this->assertInstanceOf(CookieJar::class, $signedIn);
+    }
+
+    public function test_sign_in_fail_with_location_header()
+    {
+        $response = new Response(200, ['location' => 'https://miswww1.ccu.edu.tw/alumni/alumni/login.php']);
+
+        $guzzle = Mockery::mock(ClientInterface::class);
+
+        $guzzle->shouldReceive('request')
+            ->once()
+            ->andReturn($response);
+
+        $signedIn = (new Alumni($guzzle))->signIn('A190135784', '1234');
+
+        $this->assertFalse($signedIn);
+    }
+
+    public function test_sign_in_fail_without_location_header()
+    {
+        $guzzle = Mockery::mock(ClientInterface::class);
+
+        $guzzle->shouldReceive('request')
+            ->once()
+            ->andReturn(new Response);
+
+        $signedIn = (new Alumni($guzzle))->signIn('A190135784', '1234');
+
+        $this->assertFalse($signedIn);
+    }
 }
